be84db61f234ab984ce0f9c787c77efad34a74d8
mericalp <83503845+mericalp@users.noreply.github.com>
2023-08-18 21:46:34 +0300

    Update security.tf

--- tf/security.tf
+++ tf/security.tf
@@ -1,45 +1,4 @@
 
-# resource "aws_security_group" "lb" {
-#   name        = "mern-stack-load-balancer-security-group"
-#   description = "controls access to the ALB"
-#   vpc_id      = aws_vpc.main.id
-
-#   ingress {
-#     protocol    = "tcp"
-#     from_port   = var.app_port
-#     to_port     = var.app_port
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     protocol    = "-1"
-#     from_port   = 0
-#     to_port     = 0
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-# resource "aws_security_group" "ecs_tasks" {
-#   name          = "mern-stack-task-security-group"
-#   description   = "allow inbund acces from the alb "
-#   vpc_id        = aws_vpc.main.id
-
-#   ingress {
-#     protocol     = "tcp"
-#     from_port    = var.app_port
-#     to_port      = var.app_port
-#     security_groups = [aws_security_group.lb.id]
-
-#   }
-
-#   egress { 
-#     protocol     = "-1"
-#     from_port    = 0
-#     to_port      = 0 
-#     cidr_blocks  = ["0.0.0.0/0"]
-#   }
-# }
-
 
 
 # security.tf
